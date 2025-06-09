16a9de433c16d4858a09b364879b3b7df6a34f59
Ivanowicz-pl <piartteam@gmail.com>
2025-06-10 01:18:11 +0200

    wracam z plikiem page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import BookForm from "./BookForm";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <BookForm />
+    </main>
+  );
+}
